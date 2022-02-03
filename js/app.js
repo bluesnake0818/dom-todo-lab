cef7a35463574a82dd03b6eaea44cac8a62888cf
bluesnake0818 <raidient87@gmail.com>
2022-02-03 12:52:55 -0500

    solve delete list item

--- js/app.js
+++ js/app.js
@@ -17,40 +17,29 @@ btn.addEventListener('click', function(event){
     // attach the newLi to the <ul>
     todoList.appendChild(newLi)
     }
+  // console.dir(todoList)
+  // console.log(todoList.childNodes[0].)
 })
+
 
 // Add a Reset button. After the Reset button is 
 // clicked all items in the to-do list should be 
 // removed. The input field should also be cleared 
 // so that it doesn’t hold any text.
+
+
+// why doesn't ipt.innerHTML = "" clear the value from the input box?
+// only ipt.value = "" works. 
 resetBtn.addEventListener('click', function(event) {
-
-
-
+  todoList.innerHTML = ""
+  ipt.value = ""
 })
   
   
-  
+// Delete an item on Click
 
-
-//   // make a refernce to this when building Tic Tac Toe
-// })
-
-// document.querySelector('ul').addEventListener('click',  handleClick)
-
-// //event delegation
-// function handleClick(event) {
-//   console.log(event)
-// }
-
-// document.querySelector('ul').addEventListener('click',  changeColor)
-
-// function changeColor(event) {
-//   // identify the target
-//   // adjust font color
-//   // const litoAdjust = event.target
-//   // litoAdjust.style.color = 'lime'
-//   event.target.style.color = 'lime'
-// }
+todoList.addEventListener('click', function deleteTodo(event) {
+  todoList.removeChild(event.target)
+})
 
 
